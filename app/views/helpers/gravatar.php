c262bb931ce1f4e51ce2122034bfb7a15189fca9
yandod <ando@rikezemi.com>
2009-10-18 01:56:47 -0400

    removed

--- app/views/helpers/gravatar.php
+++
@@ -1,8 +0,0 @@
-<?php
-class GravatarHelper extends AppHelper { 
-  var $helpers = array('Html');
-  function image($mail){
-    return $this->Html->image('http://www.gravatar.com/avatar/'.md5($mail).'?s=40&r=G');
-  }
-}
-?>
